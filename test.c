a031072f0389728cc77c14cb2e61df445a385c01
krl42c <karolc9991@gmail.com>
2023-11-01 17:47:21 +0100

    update test.c

--- test.c
+++ test.c
@@ -28,7 +28,11 @@ int main(void) {
     }
 
     Item *item = ht_create_item("test", "test_value");
-    
+    Item *item2 = ht_create_item("test_2", "test_value_2");
+    Item *item3 = ht_create_item("test_3", "test_value_3");
+    Item *item4 = ht_create_item("test_4", "test_value_4");
+    Item *item5 = ht_create_item("test_5", "test_value_5");
+
     // Insert
     start_time = (float) clock() / CLOCKS_PER_SEC;
     ht_insert(hash_table, item);
@@ -42,25 +46,38 @@ int main(void) {
 
     float elapsed_time_retrieve = end_time - start_time;
 
+    ht_insert(hash_table, item2);
+    ht_insert(hash_table, item3);
+    ht_insert(hash_table, item4);
+    ht_insert(hash_table, item5);
+
     printf("-----------------------------------------------\n");
     printf("Table: " MAG " %zu " RESET " size\n", table_size);
     printf("Time took to insert element: " MAG " %f " RESET "\n", elapsed_time_insert);
     printf("Time took to retrieve element: " MAG " %f " RESET "\n", elapsed_time_retrieve);
     printf("-----------------------------------------------\n");
+
     printf("Correct lookup test\n");
     printf("Expecting value " MAG "[test_value]" RESET ", found: " GRN "[%s]\n" RESET, found);
+    printf("Expecting value " MAG "[test_value_2]" RESET ", found: " GRN "[%s]\n" RESET, ht_get(hash_table,"test_2"));
+    printf("Expecting value " MAG "[test_value_3]" RESET ", found: " GRN "[%s]\n" RESET, ht_get(hash_table,"test_3"));
+    printf("Expecting value " MAG "[test_value_4]" RESET ", found: " GRN "[%s]\n" RESET, ht_get(hash_table,"test_4"));
+    printf("Expecting value " MAG "[test_value_5]" RESET ", found: " GRN "[%s]\n" RESET, ht_get(hash_table,"test_5"));
 
     printf("-----------------------------------------------\n");
-    printf("Testing collision: 8yn0iYCKYHlIj4-BwPqk=VALUE1 and GReLUrM4wMqfg9yzV3KQ=VALUE2\n");
+    printf("Testing collision: gMPflVXtwGDXbIhP73TX=VALUE1 and LtHf1prlU1bCeYZEdqWf=VALUE2\n");
 
-    Item *col1 = ht_create_item("asdsad", "v1");
-    Item *col2 = ht_create_item("8grelurm4wmqfg9yzv3kq", "VALUE2");
+    Item *col1 = ht_create_item("gMPflVXtwGDXbIhP73TX", "COL 1");
+    Item *col2 = ht_create_item("LtHf1prlU1bCeYZEdqWf", "COL 2");
     
+    ht_insert(hash_table, col1);
     ht_insert(hash_table, col2);
-    //ht_insert(hash_table, col2);
 
-    char* colide_val = ht_get(hash_table, "8grelurm4wmqfg9yzv3kq");
-    printf("8yn0iYCKYHlIj4-BwPqk=%s\n", colide_val);
+    char* colide_val1 = ht_get(hash_table, "gMPflVXtwGDXbIhP73TX");
+    char* colide_val2 = ht_get(hash_table, "LtHf1prlU1bCeYZEdqWf");
+
+    printf("Expected collide value: " MAG " [%s] " RESET ", got "GRN" [%s] " RESET "\n", "COL 1", colide_val1);
+    printf("Expected collide value: " MAG " [%s] " RESET ", got "GRN" [%s] " RESET "\n", "COL 2", colide_val2);
 
     printf("-----------------------------------------------\n");
     ht_free_item(item);
